16efca210031247468b88e79b2ae8ecbb01c7dde
weijia <weijia.canada@gmail.com>
2017-08-02 00:20:01 -0400

    config setting

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by weijiaPro on 2017/8/1.
+ */
+import React from 'react';
+import  ReactDOM from 'react-dom';
+
+class App extends React.Component{
+    render(){
+        return (<div>Hello World</div>);
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
